d2908a519216757a30ea9c6c540d670a1a3044f1
Master.G <master-g@users.noreply.github.com>
2024-08-20 21:38:23 +0800

    feat: leveling

--- crates/emukc_model/src/kc2/level.rs
+++ crates/emukc_model/src/kc2/level.rs
@@ -85,49 +85,84 @@ static SHIP_EXP_TABLE: LazyLock<Vec<i64>> = LazyLock::new(|| {
 		(1, 20000),
 		(1, 30000),
 		(1, 58500),
+		(1, 0),
 		(1, 10000),
-		(10, 1000),
-		(5, 2000),
-		(5, 3000),
-		(10, 4000),
-		(10, 5000),
-		(5, 7000),
-		(5, 8000),
-		(5, 9000),
-		(1, 10000),
-		(1, 2000),
-		(1, 3000),
-		(1, 4000),
-		(1, 5000),
-		(1, 6000),
-		(1, 7000),
-		(1, 8000),
-		(1, 9000),
-		(1, 10000),
-		(1, 13000),
-		(1, 26000),
-		(1, 39000),
-		(1, 52000),
-		(1, 65000),
-		(1, 78000),
-		(1, 84000),
-		(1, 104000),
-		(1, 123000),
-		(1, 50000),
-		(1, 100000),
-		(1, 200000),
-		(1, 400000),
+		(0, 0),     // 100
+		(10, 1000), // 110
+		(5, 2000),  // 115
+		(5, 3000),  // 120
+		(10, 4000), // 130
+		(9, 5000),  // 140
+		(5, 7000),  // 145
+		(5, 8000),  // 150
+		(5, 9000),  // 155
+		(1, 10000), // 156
+		(0, 0),
+		(1, 60000), // 157
+		(0, 0),
+		(1, 80000), // 158
+		(0, 0),
+		(1, 110000), // 159
+		(0, 0),
+		(1, 150000), // 160
+		(0, 0),
+		(1, 200000), // 161
+		(0, 0),
+		(1, 260000), // 162
+		(0, 0),
+		(1, 330000), // 163
+		(0, 0),
+		(1, 410000), // 164
+		(0, 0),
+		(1, 500000), // 165
+		(0, 0),
+		(1, 100000), // 166
+		(0, 0),
+		(1, 113000), // 167
+		(0, 0),
+		(1, 139000), // 168
+		(0, 0),
+		(1, 178000), // 169
+		(0, 0),
+		(1, 230000), // 170
+		(0, 0),
+		(1, 295000), // 171
+		(0, 0),
+		(1, 373000), // 172
+		(0, 0),
+		(1, 457000), // 173
+		(0, 0),
+		(1, 561000), // 174
+		(0, 0),
+		(1, 684000), // 175
+		(0, 0),
+		(1, 150000), // 176
+		(0, 0),
+		(1, 200000), // 177
+		(0, 0),
+		(1, 300000), // 178
+		(0, 0),
+		(1, 500000), // 179
+		(0, 0),
+		(1, 900000), // 180
 	];
 
 	let mut ship_exp_table = Vec::new();
-	let mut exp_start = 0;
+	let mut exp_now = 0;
 	let mut diff_now = 0;
 
 	for (count, diff) in sections {
+		if diff == 0 {
+			diff_now = 0;
+			if count != 0 {
+				ship_exp_table.push(exp_now);
+			}
+			continue;
+		}
 		for _ in 0..count {
 			diff_now += diff;
-			exp_start += diff_now;
-			ship_exp_table.push(exp_start);
+			exp_now += diff_now;
+			ship_exp_table.push(exp_now);
 		}
 	}
 
@@ -145,7 +180,7 @@ static SHIP_EXP_TABLE: LazyLock<Vec<i64>> = LazyLock::new(|| {
 /// A tuple of the ship level and the required exp for the next level.
 pub fn exp_to_ship_level(exp: i64) -> (i64, i64) {
 	if exp < 100 {
-		return (1, 100 - exp);
+		return (1, 100);
 	}
 	for (i, &exp_required) in SHIP_EXP_TABLE.iter().enumerate() {
 		if exp < exp_required {
@@ -153,7 +188,7 @@ pub fn exp_to_ship_level(exp: i64) -> (i64, i64) {
 		}
 	}
 
-	(175, 0)
+	(180, 0)
 }
 
 #[cfg(test)]
@@ -162,8 +197,15 @@ mod test {
 	#[test]
 	fn test_ship_exp_table() {
 		for (i, &exp) in super::SHIP_EXP_TABLE.iter().enumerate() {
-			println!("Level: {}, Exp: {}", i + 1, exp);
+			println!("lv: {}, Exp: {}", i + 2, exp);
 		}
+	}
+
+	#[test]
+	fn test_ship_lv_edges() {
+		[1_000_000, 12_000_000, 12_999_999, 13_000_000].iter().for_each(|&exp| {
+			println!("Exp: {}, {:?}", exp, super::exp_to_ship_level(exp));
+		});
 	}
 
 	#[test]
@@ -198,7 +240,7 @@ mod test {
 			}
 			exp += 100;
 
-			if new_lv == 175 {
+			if new_lv == 179 {
 				break;
 			}
 		}
