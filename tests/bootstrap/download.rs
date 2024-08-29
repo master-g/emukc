c118a074e9779a46362cec93a17ea9740e654e4d
Master.G <master-g@users.noreply.github.com>
2024-08-29 14:59:15 +0800

    feat: codex save and load

--- tests/bootstrap/download.rs
+++ tests/bootstrap/download.rs
@@ -4,10 +4,20 @@ use emukc::{
 	model::{kc2::navy::KcNavy, profile::material::MaterialConfig},
 	prelude::*,
 };
+use memory_stats::memory_stats;
+
+fn print_memory_usage() {
+	if let Some(usage) = memory_stats() {
+		let physical_memory_in_mb = usage.physical_mem as f64 / 1024.0 / 1024.0;
+		println!("Physical memory usage: {:.2} MB", physical_memory_in_mb);
+	} else {
+		println!("Failed to get memory usage");
+	}
+}
 
 fn main() {
 	let _guard = new_log_builder()
-		.with_log_level("debug")
+		.with_log_level("error")
 		.with_source_file()
 		.with_line_number()
 		.with_file_appender(std::path::PathBuf::from(".data/.emukc.log"))
@@ -19,6 +29,8 @@ fn main() {
 		let save_codex_to = dir.join("codex");
 		dir.push("temp");
 		download_all(&dir, false, Some("http://127.0.0.1:1086")).await.unwrap();
+
+		print_memory_usage();
 
 		let partial_codex = parse_partial_codex(dir).unwrap();
 		let codex = Codex {
@@ -34,6 +46,10 @@ fn main() {
 			material_cfg: MaterialConfig::default(),
 		};
 
-		codex.save(save_codex_to, true).unwrap();
+		codex.save(&save_codex_to, true).unwrap();
+
+		let _codex = Codex::load(save_codex_to).unwrap();
+
+		print_memory_usage();
 	});
 }
