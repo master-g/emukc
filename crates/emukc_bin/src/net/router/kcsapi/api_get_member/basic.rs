3b66f234a3bfb24a2e519e046716cbf227f000c1
Master.G <master-g@users.noreply.github.com>
2024-10-10 21:36:55 +0800

    feat(kcapi): basic

---
+++ crates/emukc_bin/src/net/router/kcsapi/api_get_member/basic.rs
@@ -0,0 +1,17 @@
+use axum::Extension;
+
+use crate::net::{
+	auth::GameSession,
+	resp::{KcApiResponse, KcApiResult},
+	AppState,
+};
+use emukc_internal::prelude::*;
+
+pub(super) async fn handler(
+	state: AppState,
+	Extension(session): Extension<GameSession>,
+) -> KcApiResult {
+	let pid = session.profile.id;
+	let basic = state.get_user_basic(pid).await?;
+	Ok(KcApiResponse::success(&basic))
+}
--- crates/emukc_bin/src/net/router/kcsapi/api_get_member/mod.rs
+++ crates/emukc_bin/src/net/router/kcsapi/api_get_member/mod.rs
@@ -1,6 +1,10 @@
-mod require_info;
 use axum::{routing::post, Router};
 
+mod basic;
+mod require_info;
+
 pub(super) fn router() -> Router {
-	Router::new().route("/require_info", post(require_info::handler))
+	Router::new()
+		.route("/require_info", post(require_info::handler))
+		.route("/basic", post(basic::handler))
 }
