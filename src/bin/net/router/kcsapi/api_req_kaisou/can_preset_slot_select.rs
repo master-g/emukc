4bf1898c30f80324191718a0cee6fe3d43f4a564
Master.G <master-g@users.noreply.github.com>
2024-10-29 18:21:15 +0800

    feat(kcsapi): can_preset_slot_select

--- src/bin/net/router/kcsapi/api_req_kaisou/can_preset_slot_select.rs
+++ src/bin/net/router/kcsapi/api_req_kaisou/can_preset_slot_select.rs
@@ -1,4 +1,5 @@
 use axum::Extension;
+use emukc::prelude::PresetOps;
 
 use crate::net::{
 	auth::GameSession,
@@ -7,18 +8,16 @@ use crate::net::{
 };
 
 pub(super) async fn handler(
-	_state: AppState,
-	Extension(_session): Extension<GameSession>,
+	state: AppState,
+	Extension(session): Extension<GameSession>,
 ) -> KcApiResult {
-	// TODO: implement this
-	// let pid = session.profile.id;
-	// let preset_slots = state.0.get_preset_slot(pid).await?;
-	// let flag = if preset_slots.api_preset_items.is_empty() {
-	// 	0
-	// } else {
-	// 	1
-	// };
-	let flag = 1;
+	let pid = session.profile.id;
+	let preset_slots = state.get_preset_slot(pid).await?;
+	let flag = if preset_slots.records.is_empty() {
+		0
+	} else {
+		1
+	};
 
 	let data = serde_json::json!({
 	   "api_flag": flag
