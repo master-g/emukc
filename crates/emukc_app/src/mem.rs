d04c50d369a37a16b4f60d23ae6e2b44559f288d
Master.G <master-g@users.noreply.github.com>
2025-06-18 10:42:10 +0800

    chore: allocator maintanence

--- crates/emukc_app/src/mem.rs
+++ crates/emukc_app/src/mem.rs
@@ -1,10 +1,10 @@
 #[cfg(target_os = "android")]
 #[global_allocator]
-pub static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
+pub static ALLOC: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 #[cfg(target_os = "freebsd")]
 #[global_allocator]
-pub static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
+pub static ALLOC: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 #[cfg(target_os = "ios")]
 #[global_allocator]
@@ -20,8 +20,8 @@ pub static ALLOC: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 #[cfg(target_os = "netbsd")]
 #[global_allocator]
-pub static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
+pub static ALLOC: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
 
 #[cfg(target_os = "openbsd")]
 #[global_allocator]
-pub static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
+pub static ALLOC: tivk_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
